2986be10516e1e69eba0afb08edfb570a84694d8
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-14 02:01:26 +0200

    Pass cancelTrust test

--- src/main/java/eu/trustdemocracy/social/core/interactors/relationship/trust/CancelTrust.java
+++ src/main/java/eu/trustdemocracy/social/core/interactors/relationship/trust/CancelTrust.java
@@ -1,19 +1,41 @@
 package eu.trustdemocracy.social.core.interactors.relationship.trust;
 
+import eu.trustdemocracy.social.core.entities.Relationship;
+import eu.trustdemocracy.social.core.entities.RelationshipType;
+import eu.trustdemocracy.social.core.entities.util.RelationshipMapper;
 import eu.trustdemocracy.social.core.interactors.Interactor;
 import eu.trustdemocracy.social.core.models.request.TargetRelationshipRequestDTO;
 import eu.trustdemocracy.social.core.models.response.RelationshipResponseDTO;
 import eu.trustdemocracy.social.gateways.RelationshipDAO;
+import lombok.val;
 
 public class CancelTrust implements
     Interactor<TargetRelationshipRequestDTO, RelationshipResponseDTO> {
 
+  private RelationshipDAO relationshipDAO;
+
   public CancelTrust(RelationshipDAO relationshipDAO) {
+    this.relationshipDAO = relationshipDAO;
   }
 
   @Override
   public RelationshipResponseDTO execute(
       TargetRelationshipRequestDTO targetRelationshipRequestDTO) {
-    return null;
+    val relationship = RelationshipMapper.createEntity(targetRelationshipRequestDTO);
+    relationship.setRelationshipType(RelationshipType.TRUST);
+
+    val foundRelationship = relationshipDAO.find(relationship);
+
+    if (foundRelationship == null) {
+      throw new RuntimeException("The relationship must exist to be removed");
+    }
+
+    val followRelationship = new Relationship()
+        .setOriginUser(foundRelationship.getOriginUser())
+        .setTargetUser(foundRelationship.getTargetUser())
+        .setRelationshipType(RelationshipType.FOLLOW);
+    relationshipDAO.remove(followRelationship);
+
+    return RelationshipMapper.createResponse(relationshipDAO.remove(foundRelationship));
   }
 }
