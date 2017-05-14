6b2e8af4fab08746da706e8ca7b6803ec16880c2
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-14 02:25:08 +0200

    Pass createEvent test

--- src/main/java/eu/trustdemocracy/social/gateways/mongo/MongoEventDAO.java
+++ src/main/java/eu/trustdemocracy/social/gateways/mongo/MongoEventDAO.java
@@ -1,20 +1,38 @@
 package eu.trustdemocracy.social.gateways.mongo;
 
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import eu.trustdemocracy.social.core.entities.Event;
 import eu.trustdemocracy.social.core.entities.User;
 import eu.trustdemocracy.social.gateways.EventDAO;
+import io.vertx.core.json.JsonObject;
 import java.util.List;
 import java.util.UUID;
+import lombok.val;
+import org.bson.Document;
 
 public class MongoEventDAO implements EventDAO {
 
+  private static final String EVENTS_COLLECTION = "events";
+  private MongoCollection<Document> collection;
+
   public MongoEventDAO(MongoDatabase db) {
+    this.collection = db.getCollection(EVENTS_COLLECTION);
   }
 
   @Override
   public Event create(Event event) {
-    return null;
+    event.setId(getUniqueUUID());
+
+    val document = new Document("id", event.getId().toString())
+        .append("user_id", event.getUserId().toString())
+        .append("timestamp", event.getTimestamp())
+        .append("serialized_content", event.getSerializedContent().encode());
+
+    collection.insertOne(document);
+    return event;
   }
 
   @Override
@@ -25,5 +43,30 @@ public class MongoEventDAO implements EventDAO {
   @Override
   public List<Event> getUserEvents(User user, UUID targetUserId) {
     return null;
+  }
+
+  private Event findById(UUID id) {
+    val document = collection.find(eq("id", id.toString())).first();
+    if (document == null) {
+      return null;
+    }
+
+    return buildFromDocument(document);
+  }
+
+  private UUID getUniqueUUID() {
+    UUID id;
+    do {
+      id = UUID.randomUUID();
+    } while (findById(id) != null);
+    return id;
+  }
+
+  private Event buildFromDocument(Document document) {
+    return new Event()
+        .setId(UUID.fromString(document.getString("id")))
+        .setUserId(UUID.fromString(document.getString("user_id")))
+        .setTimestamp(document.getLong("timestamp"))
+        .setSerializedContent(new JsonObject(document.getString("serialized_content")));
   }
 }
