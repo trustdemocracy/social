32a84d7be14c911b0caf0b705e203e376b1a78fe
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 06:15:06 +0200

    Add acceptFollow failing test

--- src/test/java/eu/trustdemocracy/social/endpoints/FollowControllerTest.java
+++ src/test/java/eu/trustdemocracy/social/endpoints/FollowControllerTest.java
@@ -2,6 +2,7 @@ package eu.trustdemocracy.social.endpoints;
 
 import eu.trustdemocracy.social.core.interactors.util.TokenUtils;
 import eu.trustdemocracy.social.core.models.request.OriginRelationshipRequestDTO;
+import eu.trustdemocracy.social.core.models.request.TargetRelationshipRequestDTO;
 import eu.trustdemocracy.social.core.models.response.RelationshipResponseDTO;
 import io.vertx.core.json.Json;
 import io.vertx.ext.unit.TestContext;
@@ -18,6 +19,7 @@ public class FollowControllerTest extends ControllerTest {
   private UUID originUserId = UUID.randomUUID();
   private String originUserUsername = "username";
   private UUID targetUserId = UUID.randomUUID();
+  private String targetUserUsername = "username";
 
   @Test
   public void follow(TestContext context) {
@@ -42,6 +44,52 @@ public class FollowControllerTest extends ControllerTest {
       context.assertEquals(REL_TYPE, responseRelationship.getRelationshipType().toString());
       context.assertEquals("PENDING", responseRelationship.getRelationshipStatus().toString());
       async.complete();
+    }, error -> {
+      context.fail(error);
+      async.complete();
+    });
+  }
+
+
+  @Test
+  public void acceptFollow(TestContext context) {
+    val async = context.async();
+    val followRequest = new OriginRelationshipRequestDTO()
+        .setOriginUserToken(TokenUtils.createToken(originUserId, originUserUsername))
+        .setTargetUserId(targetUserId);
+
+    val acceptRequest = new TargetRelationshipRequestDTO()
+        .setOriginUserId(originUserId)
+        .setTargetUserToken(TokenUtils.createToken(targetUserId, targetUserUsername));
+
+
+    val single = client.post(port, HOST, "/follow/")
+        .rxSendJson(followRequest);
+
+    single.subscribe(followResponse -> {
+      context.assertEquals(followResponse.statusCode(), 201);
+
+      client.post(port, HOST, "/follow/accept")
+          .rxSendJson(acceptRequest)
+          .subscribe(response -> {
+            context.assertEquals(response.statusCode(), 200);
+            context.assertTrue(response.headers().get("content-type").contains("application/json"));
+
+
+            val responseRelationship = Json
+                .decodeValue(response.body().toString(), RelationshipResponseDTO.class);
+            context.assertEquals(originUserId, responseRelationship.getOriginUserId());
+            context.assertEquals(originUserUsername, responseRelationship.getOriginUserUsername());
+            context.assertEquals(targetUserId, responseRelationship.getTargetUserId());
+            context.assertEquals(targetUserUsername, responseRelationship.getTargetUserId());
+            context.assertEquals(REL_TYPE, responseRelationship.getRelationshipType().toString());
+            context
+                .assertEquals("ACCEPTED", responseRelationship.getRelationshipStatus().toString());
+            async.complete();
+          }, error -> {
+            context.fail(error);
+            async.complete();
+          });
     }, error -> {
       context.fail(error);
       async.complete();
