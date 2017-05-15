45da2f27970d159ccf34b47c532bf6dabee00f0e
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 07:33:09 +0200

    Add findByTargetId failing test at DAO

--- src/test/java/eu/trustdemocracy/social/gateways/mongo/MongoRelationshipDAOTest.java
+++ src/test/java/eu/trustdemocracy/social/gateways/mongo/MongoRelationshipDAOTest.java
@@ -142,6 +142,49 @@ public class MongoRelationshipDAOTest {
     }
   }
 
+  @Test
+  public void findByTargetId() {
+    val userId = UUID.randomUUID();
+
+    List<Relationship> relationships = new ArrayList<>();
+    for (int i = 0; i < 30; i++) {
+      val relationship = getRandomRelationship();
+      if (i % 2 == 0) {
+        relationship.setRelationshipType(RelationshipType.FOLLOW);
+      }
+      if (i % 3 == 0) {
+        relationship.getTargetUser().setId(userId);
+        relationships.add(relationship);
+      }
+      relationshipDAO.create(relationship);
+    }
+
+
+    val followRelationships = relationships.stream()
+        .filter(r -> r.getRelationshipType() == RelationshipType.FOLLOW)
+        .collect(Collectors.toList());
+    assertNotEquals(0, followRelationships.size());
+
+    val foundFollowRelationships = relationshipDAO
+        .findByTargetId(userId, RelationshipType.FOLLOW, RelationshipStatus.PENDING);
+    assertEquals(followRelationships.size(), foundFollowRelationships.size());
+    for (Relationship relationship : followRelationships) {
+      assertTrue(foundFollowRelationships.contains(relationship));
+    }
+
+    val trustRelationships = relationships.stream()
+        .filter(r -> r.getRelationshipType() == RelationshipType.TRUST)
+        .collect(Collectors.toList());
+    assertNotEquals(0, trustRelationships.size());
+
+    val foundTrustRelationships = relationshipDAO
+        .findByTargetId(userId, RelationshipType.TRUST, RelationshipStatus.PENDING);
+    assertEquals(trustRelationships.size(), foundTrustRelationships.size());
+    for (Relationship relationship : trustRelationships) {
+      assertTrue(foundTrustRelationships.contains(relationship));
+    }
+  }
+
   private Relationship getRandomRelationship() {
     return new Relationship()
         .setOriginUser(new User().setId(UUID.randomUUID()).setUsername("originUsername"))
